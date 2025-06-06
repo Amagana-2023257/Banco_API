80aa8b4eff4d438f2e95aa18ffb4bea54f9b6264
Amagana <Amagana-2023257@kinal.edu.gt>
2025-06-06 03:42:23 -0600

    fix: Server.js/Origin

--- configs/server.js
+++ configs/server.js
@@ -19,7 +19,7 @@ const middlewares = (app) => {
   app.use(express.json());
 
   app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://banca-kinal.web.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
